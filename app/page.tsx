39c335d90326cb7da4b938f0c105157af9e5b37f
Gian Sanchez <giansanchez71@gmail.com>
2024-03-07 08:09:22 +0800

    wrangling boilerplate to use tailwind and nexjs app router

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,9 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return (
+    <div className="flex flex-1 w-full items-center justify-center">
+      <h1 className="text-blue-400">App Router</h1>
+    </div>
+  );
 }
